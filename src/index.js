f074c0dbcbe4affdcba4073a3e92ab6b12ef407c
leeheezin <gmlwls107@naver.com>
2023-04-11 02:11:44 +0900

    todolist 끝

--- src/index.js
+++ src/index.js
@@ -1,40 +1,73 @@
 import { legacy_createStore } from "redux"
 
-const add = document.getElementById("add")
-const minus = document.getElementById("minus")
-const number = document.querySelector("span")
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0
+const ADD_TODO = "ADD_TODO"
+const DELETE_TODO = "DELETE_TODO"
 
-const ADD = "ADD"
-const MINUS = "MINUS"
-
-const countModifier = (count = 0, action) => {
-  switch(action.type){
-    case ADD:
-      return count +1
-    case MINUS:
-      return count - 1
-    default:
-      return count
+const addTodo = text => {
+  return {
+    type: ADD_TODO, 
+    text 
   }
-} //데이터를 수정하는곳
-
-const countStore = legacy_createStore(countModifier)
-
-
-const onChange = () => {
-  number.innerText = countStore.getState()
 }
-
-countStore.subscribe(onChange)
-
-const handleAdd = () => {
-  countStore.dispatch({type: ADD}) //action은 반드시 object여야함, type이 있어야함
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
 }
-const handleMinus = () => {
-  countStore.dispatch({type: MINUS})
-}
+const reducer = (state = [], action) => {
+  console.log(action)
+  switch(action.type){
+    case ADD_TODO:
+      const newTodoObj = { text: action.text, id: Date.now() }
+      return [newTodoObj, ...state] 
+    case DELETE_TODO:
+      const cleaned = state.filter(toDo => toDo.id !== action.id) //delete할 object를 제외시킨 새로운array를 만듬
+      return cleaned
+    default:
+      return state
+  }
+} //state를 변형하지않고 새로운 state를 만듬
 
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+const store = legacy_createStore(reducer)
+
+store.subscribe(()=> console.log(store.getState()))
+
+const dispatchAddTodo = (text) => {
+  store.dispatch(addTodo(text))
+}
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id)
+  store.dispatch(deleteTodo(id))
+}
+const paintTodos = () =>{
+  const toDos = store.getState()
+  ul.innerHTML = "" //새로운 todo가 생기면 리스트를 비움
+  toDos.forEach(toDo => {
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    btn.innerText = "DEL"
+    btn.addEventListener("click", dispatchDeleteTodo)
+    li.id = toDo.id
+    li.innerText = toDo.text
+    li.appendChild(btn)
+    ul.appendChild(li)
+  }) // state에 각각 todo로 다시 새로운 리스트 만듬
+}
+store.subscribe(paintTodos)
+
+
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddTodo(toDo)
+};
+
+form.addEventListener("submit", onSubmit);
+
